9742206e239d8c25e26b70beb6fb299cd1455f6e
Joseph Tran <joseph.tran.p@gmail.com>
2025-01-29 16:20:01 +1100

    Fix timers not updating on reconnection

--- src/components/WebSocketContext/WebSocketContext.tsx
+++ src/components/WebSocketContext/WebSocketContext.tsx
@@ -1,9 +1,9 @@
-import { createContext, type ReactNode, useContext, useState } from 'react';
+import { createContext, type ReactNode, useCallback, useContext, useEffect, useState } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import type { SendJsonMessage } from 'react-use-websocket/dist/lib/types';
 import { useRoomStore } from 'stores/useRoomStore';
 import { RoomCheckMessage } from 'types/ClientMessageTypes';
-import { ServerMessageTypes } from 'types/ServerMessageTypes';
+import { RoomValidityMessage, ServerMessageTypes } from 'types/ServerMessageTypes';
 
 interface WebSocketContextType {
   lastJsonMessage: ServerMessageTypes;
@@ -21,11 +21,13 @@ const WebSocketContext = createContext<WebSocketContextType | undefined>(undefin
 export const WebSocketProvider = ({ children }: WebSocketProviderProps) => {
   const getRoomCode = useRoomStore((state) => state.getRoomCode);
   const [connectToWS, setConnectToWS] = useState<boolean>(true);
+  const [isFirstConnection, setIsFirstConnection] = useState<boolean>(true);
 
   const { lastJsonMessage, sendJsonMessage, readyState } = useWebSocket<ServerMessageTypes>(
     import.meta.env.VITE_WS_URL!,
     {
       shouldReconnect: () => true,
+      reconnectAttempts: 10,
       reconnectInterval: (attemptNumber) => Math.min(Math.pow(2, attemptNumber) * 1000, 10000),
       onOpen: () => {
         const roomCode = getRoomCode();
@@ -36,10 +38,39 @@ export const WebSocketProvider = ({ children }: WebSocketProviderProps) => {
             accessId: roomCode,
           } as RoomCheckMessage);
         }
+
+        if (isFirstConnection) {
+          setIsFirstConnection(false);
+        } else {
+          console.log('hello');
+        }
       },
     },
     connectToWS,
   );
+
+  const handleRoomCheck = useCallback(
+    (data: RoomValidityMessage) => {
+      if (data.valid && !isFirstConnection) {
+        sendJsonMessage({ type: 'subscribe', accessId: getRoomCode() });
+      }
+    },
+    [sendJsonMessage, getRoomCode, isFirstConnection],
+  );
+
+  useEffect(() => {
+    if (!lastJsonMessage) {
+      return;
+    }
+
+    switch (lastJsonMessage.type) {
+      case 'roomValidity':
+        handleRoomCheck(lastJsonMessage);
+        break;
+      default:
+        return;
+    }
+  }, [lastJsonMessage, handleRoomCheck]);
 
   const reconnect = () => {
     if (readyState !== ReadyState.OPEN) {
